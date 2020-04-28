3cf8c64b65b8e16e486347558f335bb95183e98b
Naina Tejani <naina.tejani@gmail.com>
2020-04-28 05:14:39 -0400

    made regressions

--- final_project_shiny/app.R
+++ final_project_shiny/app.R
@@ -5,67 +5,100 @@ library(shinythemes)
 library(tidyverse)
 library(ggplot2)
 library(ggthemes)
+library(plotly)
 
 # load necessary dependencies
 
-map_1910 <- read_rds("map_1910.rds")
-map_1920 <- read_rds("map_1920.rds")
-map_1930 <- read_rds("map_1930.rds")
-map_1940 <- read_rds("map_1940.rds")
+
+CO2Plot <- read_rds("co2_temp_plot.rds")
+emissions_and_oil_consumption <- read_rds("C02_Emissions_And_OIL.rds")
+country_names <- read_rds("country_names.rds")
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(theme = shinytheme("sandstone"),
     # Add line breaks for aesthetic purposes
     br(),
     navbarPage("Climate Change: The Data Speaks The Truth", 
-               tabPanel("Heat Map Visualizations",                        
-                        h3("Change in Global Temperatures by Decade"),
-                        strong("The blue color becoming lighter indicates temperature rise."),
-                        
-                        br(),
-                        br(),
-                        
-                        sidebarPanel(
-                          radioButtons(inputId = "heat_year",
+      tabPanel("Heat Map Visualizations",                        
+              h3("Change in Global Temperatures by Decade"),
+              strong("The blue color becoming lighter indicates temperature rise."),
+              br(),
+              br(),
+              sidebarPanel(radioButtons(inputId = "heat_year",
                                        label = "Select a decade:",
-                                       choices = c("1910","1920","1930","1940"))),
-                        
-                        mainPanel(
-                          
-                          
-                          plotOutput("heatmap"),
-                          
-                          br()
-                          
-                        )),
-  
-                        
-              tabPanel("Role of CO2 Emissions"),
-              tabPanel("Who Is Responsible?")
-              
+                                       choices = c("1910","1920","1930","1940",
+                                                   "1950","1960","1970","1980",
+                                                   "1990","2000","2010","2014"))
+                           ),
+              mainPanel(imageOutput("heatmap"))
               ),
-    
-
+      tabPanel("Effect of CO2 Emissions",
+               h3("How does change in CO2 affect global temperature?"),
+               br(),
+               br(),
+              mainPanel(plotOutput("CO2plot"))
+              ),
+      tabPanel("Trends By Country",
+               h3("Relation Between Oil Consumption and CO2 Emissions"),
+               br(),
+               br(),
+               sidebarPanel(
+                 selectInput(
+                   "country", "Select a Country", country_names
+                 )
+               ),
+               plotOutput("countrywisePlot")
+                         
+                       
+               )
+    )
 )
+       
+
+
 
 # Define server logic required to draw a histogram
-server <- function(input, output) {
-  #Output for historical heat day event map in the US
+server <- function(input, output, session) {
+
   
-  output$heatmap <- renderPlot({
-    if(input$heat_year == "1910"){
-      map_1910
-    }
-    else if(input$heat_year == "1920"){
-      map_1920
-    }
-    else if(input$heat_year == "1930"){
-      map_1930
-    }
-    else if(input$heat_year == "1940"){
-      map_1940
-    }
+  output$heatmap <- renderImage({
+    width  <- session$clientData$output_heatmap_width
+    height <- session$clientData$output_heatmap_height
+    
+    pixelratio <- session$clientData$pixelratio
+    
+    filename <- normalizePath(file.path('./maps',
+                                        paste('map', input$heat_year, '.jpeg', sep='')))
+    # Return a list containing the filename and alt text
+    filename
+    list(src = filename,
+         alt = paste("Image number", input$heat_year),
+         height = height+200,
+         width = width)
+  }, deleteFile = FALSE)
+  
+  output$CO2plot <- renderPlot({
+    CO2Plot
   })
+  output$countrywisePlot <- renderPlot({
+    "hello i am here"
+    plot <- emissions_and_oil_consumption %>%
+               filter(country_name == input$country) %>% 
+               ggplot(aes(x = energy_use, 
+                          y = emissions_per_capita)) +
+               geom_point() 
+               # theme_classic() + 
+               # labs(title = "Change in CO2 Emissions with Rising Oil Consumption", 
+               #              x = "Energy Use(kg of oil equivalent per capita)",
+               #              y = "Emissions of CO2(metric tons per capita)"
+               #         )
+    plot
+    
+
+
+  })
+
+  
 }
 
 # Run the application 
